2aa10078350b3fa6304a7dd88f84831943d30726
TecnoBase <ganilsong87@gmail.com>
2024-08-31 15:50:18 +0100

    status

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include("app.urls")),
+    path('', include("app.urls")),
     
 ]
